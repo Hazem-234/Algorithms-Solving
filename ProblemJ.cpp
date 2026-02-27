be3be9f05b200741a60f3f9dfce1d17cf2e967a8
Hazem Elrefaie <zoma.elrefaie333@gmail.com>
2026-02-27 20:47:50 +0200

    Factorial

---
+++ ProblemJ.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int Factorial(int N)
+{
+    if(N==0 || N==1)
+    {
+        return 1;
+    }
+
+    return N * Factorial(N-1);
+}
+int main()
+{
+    int N;
+    cin>>N;
+    cout<<Factorial(N);
+    system("pause");
+    return 0;
+}
