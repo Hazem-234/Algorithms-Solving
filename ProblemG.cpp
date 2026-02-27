0bf8b514ef719a3d967ce67a87c63c23ba3e58c4
Hazem Elrefaie <zoma.elrefaie333@gmail.com>
2026-02-27 20:45:16 +0200

    Pyramid

---
+++ ProblemG.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+//
+void stars(int Stcount)
+{
+    if(Stcount==0) return;
+    cout<<"*";
+    stars(Stcount-1);
+}
+void spaces(int Scount)
+{
+    if(Scount==0) return;
+    cout<<" ";
+    spaces(Scount-1);
+}
+void Pyramid(int rows, int total)
+{
+    if(rows == total) return;
+    spaces(total-rows-1);
+    stars((2*rows)+1);
+    cout<<endl;
+    Pyramid(rows + 1, total);
+}
+int main()
+{
+    int N;
+    cin>>N;
+    Pyramid(0, N);
+    system("pause");
+    return 0;
+}
